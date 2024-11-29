3c4bde2d946162f1e1eb150eba92ffa0b25744b0
metaguji <59725664+metaguji@users.noreply.github.com>
2024-11-29 21:51:54 +0800

    Implement mock recent transactions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,54 +1,77 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeScreen() {
+  const mockData = [
+    { date: "01/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "02/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "03/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "04/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "05/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "06/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "07/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "08/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "09/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "10/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "11/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "12/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "13/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "14/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "15/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "16/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "17/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "18/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "19/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+    { date: "20/10/24", amount: "10.95", description: "Latte", type: "CREDIT" },
+  ];
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/partial-react-logo.png")}
           style={styles.reactLogo}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="subtitle">Recent Transactions</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+        <View
+          style={{
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+          }}
+        >
+          <Text>Date</Text>
+          <Text>Description</Text>
+          <Text>Amount</Text>
+          <Text>Type</Text>
+        </View>
+        <View>
+          {mockData.map((d, i) => {
+            return (
+              <View
+                style={{
+                  flexDirection: "row",
+                  flexWrap: "wrap",
+                  justifyContent: "space-around",
+                }}
+              >
+                <Text key={i}>{d.date}</Text>
+                <Text key={i}>{d.description}</Text>
+                <Text key={i}>{d.amount}</Text>
+                <Text key={i}>{d.type}</Text>
+              </View>
+            );
+          })}
+        </View>
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -56,8 +79,8 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -69,6 +92,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
